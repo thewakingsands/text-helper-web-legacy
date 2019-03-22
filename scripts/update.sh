56b6f0a37234c64daca054a5e16265ba7e8f8e52
oott123 <git@public.oott123.com>
2019-03-22 23:12:11 +0800

    update script

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+set -e
+echo "Installing packages ..."
 docker run -it --rm -v `pwd`:/app -w /app node:lts yarn
+echo "Buliding ..."
 docker run -it --rm -v `pwd`:/app -w /app node:lts yarn build
+echo "Updating directory ..."
 docker run -it --rm -v `pwd`:/app --entrypoint=/usr/bin/rsync eeacms/rsync -a --delete /app/build/ /app/dist/
