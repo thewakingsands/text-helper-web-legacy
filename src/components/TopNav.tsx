f73761ac7c24c5a7fe16af9525ff7cf14e0b5a7e
oott123 <git@public.oott123.com>
2019-05-28 21:28:13 +0800

    update to 4.5

--- src/components/TopNav.tsx
+++ src/components/TopNav.tsx
@@ -66,7 +66,7 @@ function MoreToolsMenu(props: { setShowAdvanced: (state: boolean) => void }) {
 
   return (
     <Menu>
-      <Menu.Item disabled icon="code" text="数据版本：4.45" />
+      <Menu.Item disabled icon="code" text="数据版本：4.5" />
       <Menu.Item
         icon="filter-list"
         text="高级搜索"
